cc0ea1ef43a01b94d29f038adb652d50231fc7ac
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-10-21 11:12:19 +0200

    validation number range and regular expression

--- src/com/manu/springdemo/mvc/CustomerController.java
+++ src/com/manu/springdemo/mvc/CustomerController.java
@@ -37,8 +37,13 @@ public class CustomerController {
 			BindingResult theBindingResult) {
 		
 		// log the input data
-				System.out.println("theStudent: " + theCustomer.getFirstName()
+		System.out.println("theStudent: " + theCustomer.getFirstName()
 									+ " " + theCustomer.getLastName());
+		
+		System.out.println("Binding result: " + theBindingResult);
+		
+		System.out.println("\n\n\n\n");
+		
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
 		}
